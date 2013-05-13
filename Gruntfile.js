611c4f9faddc5a62a01790cfffb8ede87f4ab883
Greg Bergé <neoziro@gmail.com>
2013-05-13 16:07:23 +0200

    update grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -28,6 +28,6 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-simple-mocha');
 
-  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('default', ['simplemocha', 'jshint']);
   grunt.registerTask('test', ['simplemocha']);
 };
